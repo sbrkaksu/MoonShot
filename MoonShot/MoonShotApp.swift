d314a284c900b6b1afeabf18d79754c688289c45
Berk Aksu <berkaksu79@gmail.com>
2024-01-10 18:20:11 +0300

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Berk Aksu on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
